0f5db8739373537fdb68e13fb95a3b5bb99dbc26
Pablo Martín <goinnn@gmail.com>
2014-04-09 21:11:13 +0200

    Adding first version of index and followers views

--- pitble/pitble/pitapp/urls.py
+++ pitble/pitble/pitapp/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('pitble.pitapp.views',
     url(r'^$', 'index', name='index'),
+    url(r'^followers/$', 'followers', name='followers'),
 )
--- pitble/pitble/pitapp/views.py
+++ pitble/pitble/pitapp/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return render_to_response('pitapp/index.html',
+                              {},
+                              context_instance=RequestContext(request))
+
+
+# Create your views here.
+def followers(request):
+    return render_to_response('pitapp/followers.html',
+                              {},
+                              context_instance=RequestContext(request))
