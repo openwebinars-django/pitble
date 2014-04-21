7f9c05af18d113624705116e224d2f5741cbf8ba
Pablo Martín <goinnn@gmail.com>
2014-04-21 21:19:21 +0200

    Fix the class problem. Now you can add an user from admin site

--- pitble/pitble/pitapp/forms.py
+++ pitble/pitble/pitapp/forms.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
-from pitble.pitapp.models import User
+from django.contrib.auth import get_user_model
 
 
 class PitbleUserCreationForm(UserCreationForm):
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ("username",)
+
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            get_user_model()._default_manager.get(username=username)
+        except get_user_model().DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
 
 
 class PitbleUserChangeForm(UserChangeForm):
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = '__all__'
