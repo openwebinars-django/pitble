7d276d7f15cce96f2026ae583c909637bd3e9697
Pablo Martín <goinnn@gmail.com>
2014-04-22 20:42:27 +0200

    Finishing the login view

--- pitble/pitble/pitapp/views.py
+++ pitble/pitble/pitapp/views.py
@@ -1,8 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth import login
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
 from pitble.pitapp.forms import SignInForm
 
@@ -28,6 +30,7 @@ def sign_in(request):
     if form.is_valid():
         user = form.user
         login(request, user)
+        messages.add_message(request, messages.INFO, _('Welcome %s' % user.username))
         return HttpResponseRedirect(reverse('index'))
     return render_to_response('pitapp/sign-in.html',
                               {'form': form,},
--- pitble/pitble/settings.py
+++ pitble/pitble/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pitble.pitapp',
+    'bootstrap3'
 )
 
 MIDDLEWARE_CLASSES = (
