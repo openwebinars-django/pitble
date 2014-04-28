c647a1cfdfefaf85dbfd9526310e5ae1027c7669
Pablo Martín <goinnn@gmail.com>
2014-04-28 21:26:11 +0200

    Fixing the 6th class error: Now works the set language action

--- example/example/settings.py
+++ example/example/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'pitble.middleware.LocaleMiddleware',
 )
 
